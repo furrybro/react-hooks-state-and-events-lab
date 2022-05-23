7c0403a2113d0fe0a17a62026f54d2776f6ffe5c
Jeena Park <jeenapark15@gmail.com>
2022-05-23 09:14:47 -0400

    commit tree 5792fee35b0ffcd8cd5de0d73ad6fe8ffd035676

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addCart, setAddCart] = useState(true);
+
+  function handleAddCart() {
+    setAddCart((addCart) => !addCart )
+  }
+
+  const cartStatus = addCart ? "" : "in-cart";
+
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddCart} className="add">{addCart ? "Add to Cart": "Remove From Cart"}</button>
     </li>
   );
 }
